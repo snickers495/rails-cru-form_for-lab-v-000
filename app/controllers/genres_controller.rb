e0367cb5f5ed8596e66505f5843efea7a268613a
Lisa Lee <snickers495@gmail.com>
2017-12-02 22:49:00 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,16 +1,26 @@
 class GenresController < ApplicationController
   def show
+    @genre = Genre.find(params[:id])
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
   end
 
   def new
+    @genre = Genre.new
   end
 
   def create
   end 
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
