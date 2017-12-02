8b8c4846cd5b886a8c208763effb8a55829c80fe
Lisa Lee <snickers495@gmail.com>
2017-12-02 22:52:26 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,8 +15,9 @@ class SongsController < ApplicationController
   def create
   end
 
-  private 
+  private
 
-  def song_params 
+  def song_params
     params.require(@song).permit(:name, :artist_id, :genre_id)
+  end
 end
