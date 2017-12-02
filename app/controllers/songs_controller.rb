6236285126ca6de0c1a2fbc048c08098956ad90a
Lisa Lee <snickers495@gmail.com>
2017-12-02 22:58:00 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,6 +2,7 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
     @genre = Genre.find(@song.genre_id)
+    @artist = Artist.find(@song.artist_id)
   end
 
   def edit
